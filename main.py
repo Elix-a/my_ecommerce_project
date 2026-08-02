45bd8f16fe3be244856e7ed7a33cc459171e1cca
Alexandr Romanov <elix.a@ya.ru>
2026-08-02 12:33:44 +0300

    Добавлены магические методы __str__, __add__

---
+++ main.py
@@ -0,0 +1,33 @@
+from src.classes import Category, Product
+
+if __name__ == "__main__":
+    product1 = Product(
+        "Samsung Galaxy S23 Ultra",
+        "256GB, Серый цвет, 200MP камера",
+        180000.0,
+        5,
+    )
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    print(product1)
+    print(product2)
+    print(product3)
+
+    print(product1 + product2)
+    print(product2 + product3)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, " "но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+    print(category1)
+
+    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    category1.add_product(product4)
+    print(category1)
+
+    print(Category.category_count)
+    print(Category.product_count)
